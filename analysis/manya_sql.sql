e01de4937bd3c0af711ec8b1fcf9e1308ab8e52a
manya <manyashreedeo@gmail.com>
2025-10-18 21:52:15 +0530

    perfomed exploratory analysis

--- analysis/manya_sql.sql
+++ analysis/manya_sql.sql
@@ -17,4 +17,28 @@ SELECT * FROM telecomm;
 
 -- 7.Does the payment method (e.g., electronic check, credit card, bank transfer) affect churn?Identifies if convenience or payment habits correlate with churn.(OMIT)
 
+
+-- CHURN AND AGE
+SELECT age,COUNT(churn) as churn_rate
+FROM telecomm
+WHERE churn=1
+GROUP BY age;
+
+-- CHURN AND SALARY
+SELECT COUNT(churn) AS churn_rate
+FROM telecomm
+WHERE churn=1 AND estimated_salary>( SELECT AVG(estimated_salary) FROM telecomm);
+
+-- CHURN AND GENDER
+SELECT COUNT(churn) AS churn_rate, gender
+FROM telecomm
+WHERE churn=1
+GROUP BY gender;
+
+-- CHURN AND no_of_dependants
+SELECT num_dependents,COUNT(churn) AS churn_rate
+FROM telecomm
+WHERE churn=1
+GROUP BY num_dependents;
+
     
